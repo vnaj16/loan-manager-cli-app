b0bf42484fca08768f6606a372bf64f91ba9d5c5
Arthur Valladares Nole <arthurvalladares@belcorp.biz>
2023-01-15 11:33:52 -0500

    Finish Set Up of CLI app with Type and Testing with Pytest

--- loan_manager/__main__.py
+++ loan_manager/__main__.py
@@ -1,1 +1,10 @@
 """Loan Manager CLI App entry point script."""
+# loan_manager/__main__.py
+
+from loan_manager import cli, __app_name__
+
+def main():
+    cli.app(prog_name=__app_name__)
+
+if __name__ == "__main__":
+    main()
