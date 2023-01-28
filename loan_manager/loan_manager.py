f2184b62b63ee65e665660b69b058e5c3edafca8
Arthur Valladares Nole <arthurvalladares@belcorp.biz>
2023-01-28 15:36:12 -0500

    Add test_add method in Tests

--- loan_manager/loan_manager.py
+++ loan_manager/loan_manager.py
@@ -2,7 +2,8 @@
 # loan_manager\loan_manager.py
 
 from pathlib import Path
-from typing import Any, Dict, NamedTuple
+from typing import Any, Dict, List, NamedTuple
+from loan_manager import DB_READ_ERROR
 
 from loan_manager.database import DatabaseHandler
 
@@ -13,3 +14,55 @@ class CurrentLoan(NamedTuple):
 class LoanRepository:
     def __init__(self, db_path: Path) -> None:
         self._db_handler = DatabaseHandler(db_path)
+
+    def add(self, description: List[str], amount: float) -> CurrentLoan:
+        """Add a new loan to the database."""
+        description_text = " ".join(description)
+        if not description_text.endswith("."):
+            description_text += "."
+        todo = {
+            "Description": description_text,
+            "Amount": amount,
+            "Paid": False,
+        }
+        read = self._db_handler.read_loans()
+        if read.error == DB_READ_ERROR:
+            return CurrentLoan(todo, read.error)
+        read.loan_list.append(todo)
+        write = self._db_handler.write_loans(read.loan_list)
+        return CurrentLoan(todo, write.error)
+
+    def get_todo_list(self) -> List[Dict[str, Any]]:
+        """Return the current loan list."""
+        read = self._db_handler.read_loans()
+        return read.loan_list
+
+    # def set_done(self, todo_id: int) -> CurrentLoan:
+    #     """Set a to-do as done."""
+    #     read = self._db_handler.read_todos()
+    #     if read.error:
+    #         return CurrentTodo({}, read.error)
+    #     try:
+    #         todo = read.todo_list[todo_id - 1]
+    #     except IndexError:
+    #         return CurrentTodo({}, ID_ERROR)
+    #     todo["Done"] = True
+    #     write = self._db_handler.write_todos(read.todo_list)
+    #     return CurrentTodo(todo, write.error)
+
+    # def remove(self, todo_id: int) -> CurrentTodo:
+    #     """Remove a to-do from the database using its id or index."""
+    #     read = self._db_handler.read_todos()
+    #     if read.error:
+    #         return CurrentTodo({}, read.error)
+    #     try:
+    #         todo = read.todo_list.pop(todo_id - 1)
+    #     except IndexError:
+    #         return CurrentTodo({}, ID_ERROR)
+    #     write = self._db_handler.write_todos(read.todo_list)
+    #     return CurrentTodo(todo, write.error)
+
+    # def remove_all(self) -> CurrentTodo:
+    #     """Remove all to-dos from the database."""
+    #     write = self._db_handler.write_todos([])
+    #     return CurrentTodo({}, write.error)
--- tests/test_loan_manager.py
+++ tests/test_loan_manager.py
@@ -1,8 +1,17 @@
 # tests/test_loan_manager.py
+import json
 
+import pytest
 from typer.testing import CliRunner
 
-from loan_manager import __app_name__, __version__, cli
+from loan_manager import (
+    DB_READ_ERROR,
+    SUCCESS,
+    __app_name__,
+    __version__,
+    cli,
+    loan_manager,
+)
 
 runner = CliRunner()
 
@@ -10,3 +19,51 @@ def test_version():
     result = runner.invoke(cli.app, ["--version"])
     assert result.exit_code == 0
     assert f"{__app_name__} v{__version__}\n" in result.stdout
+
+@pytest.fixture
+def mock_json_file(tmp_path):
+    todo = [{"Description": "Netflix January", "Amount": 24.9, "Paid": False}]
+    db_file = tmp_path / "loan.json"
+    with db_file.open("w") as db:
+        json.dump(todo, db, indent=4)
+    return db_file
+
+test_data1 = {
+    "description": ["Payment", "Energy", "Service"],
+    "amount": 150,
+    "loan": {
+        "Description": "Payment Energy Service.",
+        "Amount": 150,
+        "Paid": False,
+    },
+}
+test_data2 = {
+    "description": ["Payment", "Water", "Service"],
+    "amount": 60,
+    "loan": {
+        "Description": "Payment Water Service.",
+        "Amount": 60,
+        "Paid": False,
+    },
+}
+
+@pytest.mark.parametrize(
+    "description, amount, expected",
+    [
+        pytest.param(
+            test_data1["description"],
+            test_data1["amount"],
+            (test_data1["loan"], SUCCESS),
+        ),
+        pytest.param(
+            test_data2["description"],
+            test_data2["amount"],
+            (test_data2["loan"], SUCCESS),
+        ),
+    ],
+)
+def test_add(mock_json_file, description, amount, expected):
+    loan_repository = loan_manager.LoanRepository(mock_json_file)
+    assert loan_repository.add(description, amount) == expected
+    read = loan_repository._db_handler.read_loans()
+    assert len(read.loan_list) == 2
